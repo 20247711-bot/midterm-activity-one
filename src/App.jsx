6847182a1fc701f3424d3fc5c55d1be0868c2f11
mendoza_m <mendoza_m@ca01.ub.lan>
2026-03-09 11:13:00 +0800

    Cleaned default installation

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,6 @@ import NavBar from './components/navbar';
 import About from './pages/About';
 import { Routes, Route } from 'react-router';
 
-const App = () => { 
-  // const [count, setCount] = useState(0)
-  const bookNumber = 1;
 
-  return (
-    <> {/* this is called fragment */}
-      <NavBar />
-      <div>
-        <div>
-          <Routes>
-            <Route path='/' element={<Home />}>Home</Route>
-            <Route path='/about' element={<About />}>About</Route>
-          </Routes>
-        </div>
-        <Home />
-      </div>
-    </>
-  )
-}
 
 export default App
