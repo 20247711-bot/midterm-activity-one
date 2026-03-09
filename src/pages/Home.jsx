3e75b7312d55e611d1c72fec49f479a52813bf39
mendoza_m <mendoza_m@ca01.ub.lan>
2026-03-09 11:32:55 +0800

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,13 @@ import ProductCard from "../components/ProductCard";
 import {useState, useEffect} from "react";
 import {getProducts} from "../services/api";
 
+function Home() {
+  return (
+    <div className="container mt-4">
+      <h2>Welcome to Student Dashboard</h2>
+      <p>This app demonstrates React Router, useState, and useEffect.</p>
+    </div>
+  )
+}
 
-
-export default Home;
+export default Home
