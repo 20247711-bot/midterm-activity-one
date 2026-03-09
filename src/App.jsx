2d49a0634995c19ff2eddc761954aed9b78ce4de
mendoza_m <mendoza_m@ca01.ub.lan>
2026-03-09 12:36:26 +0800

    Created the about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Students from "./pages/Students";
+import About from "./pages/About";
 import StudentDetails from "./pages/StudentDetails";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/students" element={<Students />} />
         <Route path="/students/:id" element={<StudentDetails />} />
+        <Route path="/About" element={<About />} />
       </Routes>
     </>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div className="container mt-4">
+      <h2>About</h2>
+      <p>This project demonstrates routing and React hooks.</p>
+    </div>
+  )
+}
+
+export default About
