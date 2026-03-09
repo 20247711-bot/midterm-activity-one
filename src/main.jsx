f66db0e3a5992c5c41db102f027568ea587cd1f4
mendoza_m <mendoza_m@ca01.ub.lan>
2026-03-09 11:23:52 +0800

    Configured the react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </BrowserRouter>
 )
